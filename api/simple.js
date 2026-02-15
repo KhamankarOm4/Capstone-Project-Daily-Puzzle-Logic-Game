6f92b089723337e3f7546c8d2025a230570cb4a6
KhamankarOm4 <khamankaros@rknec.edu>
2026-02-15 11:58:23 +0530

    Fix: Remove test functions to fit free tier

--- api/simple.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({
-        msg: "Vercel API is working!",
-        query: req.query,
-        env: process.env.NODE_ENV
-    });
-}
