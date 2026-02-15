cc0d7dab7c6e7a8af68b5bc7de23e119aa4b8c8c
KhamankarOm4 <khamankaros@rknec.edu>
2026-02-15 11:28:42 +0530

    Fix: Remove unused import

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 import Button from '../components/ui/Button';
-import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
     const handleGoogleLogin = () => {
